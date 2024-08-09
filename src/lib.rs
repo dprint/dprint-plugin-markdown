22659b7fae5f7e646158a16df1c76059d3fc8d80
David Sherret <dsherret@gmail.com>
2024-08-09 19:52:58 -0400

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
-extern crate dprint_core;
-extern crate pulldown_cmark;
-extern crate regex;
+#![allow(clippy::bool_assert_comparison)]
+#![allow(clippy::bool_to_int_with_if)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::if_same_then_else)]
+#![allow(clippy::vec_init_then_push)]
+#![allow(clippy::type_complexity)]
+#![allow(clippy::needless_lifetimes)]
+#![deny(clippy::disallowed_methods)]
+#![deny(clippy::disallowed_types)]
+#![deny(clippy::print_stderr)]
+#![deny(clippy::print_stdout)]
 
 pub mod configuration;
 mod format_text;
