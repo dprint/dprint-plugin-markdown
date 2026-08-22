2e769575fb333593713cf9d754e6724446d6e52f
Bryan A. Jones <2125584+bjones1@users.noreply.github.com>
2026-08-22 20:59:16 +0500

    chore: lint and format (#202)

--- src/generation/generate.rs
+++ src/generation/generate.rs
@@ -1160,7 +1160,9 @@ fn gen_list(list: &List, is_alternate: bool, context: &mut Context) -> PrintItem
       };
       let indent_increment = match context.configuration.list_indent_kind {
         crate::configuration::ListIndentKind::CommonMark => (prefix_text.chars().count() + 1) as u32,
-        crate::configuration::ListIndentKind::PythonMarkdown => std::cmp::max(prefix_text.chars().count() as u32 + 1, 4),
+        crate::configuration::ListIndentKind::PythonMarkdown => {
+          std::cmp::max(prefix_text.chars().count() as u32 + 1, 4)
+        }
       };
       context.indent_level += indent_increment;
       items.push_string(prefix_text);
